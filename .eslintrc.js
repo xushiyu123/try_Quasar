b9067bfbb04dcb081194be9359f0a707d979c7d7
xushiyu123 <1633813476@qq.com>
2020-09-26 15:00:24 +0800

    表格数据绑定错误更正

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,6 +67,7 @@ module.exports = {
 
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "space-before-function-paren": 0
   }
 }
